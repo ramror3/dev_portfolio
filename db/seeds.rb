9bb6cf7d7cb8f8e2488dd15709a7cd77bf876c9f
ramror3 <ramesh.sabbanis3@gmail.com>
2019-07-28 21:32:05 +0530

    created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "It has changed the way I look at myself and the world. I am beginning to feel the oneness of all the joy of existence. Many of the negative emotions are fading with the realization that it is all coming from me, not from external situations"
+	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Inner Engineering",
+		body: "So much depth and insight was offered to look at my own mind and see how the same thought process and emotions that were capable of causing me misery can be a miraculous instrument to create my life joyously.",
+		main_image: "https://via.placeholder.com/350x150",
+		thumb_image: "https://via.placeholder.com/650x350"
+	)
+end
