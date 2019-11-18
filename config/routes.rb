c93cd9b6ef32d56e93ab5779b1a7305f783151db
ramror3 <ramesh.sabbanis3@gmail.com>
2019-11-18 18:45:41 +0530

    worked on toggle status with enum

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+ 
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
 
 end
