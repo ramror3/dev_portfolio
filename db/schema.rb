879bd1ae11b016cf529dd53832af8e6defad2398
ramror3 <ramesh.sabbanis3@gmail.com>
2019-11-18 19:25:20 +0530

    build relations between the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191118061037) do
+ActiveRecord::Schema.define(version: 20191118134505) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 20191118061037) do
     t.datetime "updated_at",                           null: false
     t.string   "slug"
     t.integer  "status",                   default: 0
+    t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+    t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -50,4 +52,11 @@ ActiveRecord::Schema.define(version: 20191118061037) do
     t.datetime "updated_at",       null: false
   end
 
+  create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
